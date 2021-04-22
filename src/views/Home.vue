d2f8cd49d896a6e923ca0ffa2094b2841161e01a
2391477260 <2391477260@qq.com>
2021-04-22 20:52:17 +0800

    'init'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg=" to Your Vue.js App"/>
+    <HelloWorld msg=" to "/>
   </div>
 </template>
 
